0df486ddca26bff3dd2d463ed3f4dbcd040520a2
Sai Krishna Perugu <p.saikrishna355@gmail.com>
2025-07-07 13:39:07 +0530

    Just removed space for clear code

--- src/main/java/com/saikrishna/git_project/GitProjectApplication.java
+++ src/main/java/com/saikrishna/git_project/GitProjectApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GitProjectApplication.class, args);
 	}
